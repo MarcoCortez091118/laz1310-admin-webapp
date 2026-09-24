c77da61dc3d00de3fba53e7954151ed8181ae236
MarcoCortez091118 <127135495+MarcoCortez091118@users.noreply.github.com>
2026-09-24 17:30:53 -0600

    feat: add src/main.tsx

---
+++ src/main.tsx
@@ -0,0 +1,14 @@
+import { RouterProvider } from "@tanstack/react-router";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { AppProviders } from "./app/AppProviders";
+import { router } from "./router";
+import "./styles.css";
+
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <AppProviders>
+      <RouterProvider router={router} />
+    </AppProviders>
+  </StrictMode>,
+);
